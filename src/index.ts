ab7b5c6609591f3c96cc0b54c5fe5ff3fc398f08
DLaptiev <akiosarkiz@gmail.com>
2024-01-15 20:29:43 +0100

    add dotenv support

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+// setup environment
+require("dotenv").config();
 
-const app = new Hono()
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
